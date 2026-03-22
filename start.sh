03e16f3bbd52f0394d3ccb45e4a469edd78a52fc
hamalainenmiska100-hue <hamalainen.miska100@gmail.com>
2026-03-22 08:35:58 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,18 +3,19 @@
 RTMP="rtmp://live.restream.io/live/re_11414527_eventd786409fc869479fb2a580026af0243c"
 
 while true; do
-  ffmpeg -re \
-    -f concat -safe 0 -protocol_whitelist "file,http,https,tcp,tls" \
-    -i videos.txt \
-    -vf "fps=60,scale=720:1280:force_original_aspect_ratio=decrease,\
-pad=720:1280:(ow-iw)/2:(oh-ih)/2" \
-    -c:v libx264 -preset veryfast -tune zerolatency \
-    -b:v 2200k -maxrate 2200k -bufsize 4400k \
-    -g 120 -pix_fmt yuv420p \
-    -c:a aac -b:a 128k \
-    -ar 44100 \
-    -f flv "$RTMP"
+  while read URL; do
+    echo "Streaming $URL"
 
-  echo "Restarting clean loop..."
-  sleep 1
+    ffmpeg -re -fflags +genpts -i "$URL" \
+      -vf "fps=60,scale=720:1280:force_original_aspect_ratio=decrease,\
+pad=720:1280:(ow-iw)/2:(oh-ih)/2" \
+      -c:v libx264 -preset veryfast -tune zerolatency \
+      -b:v 2200k -maxrate 2200k -bufsize 4400k \
+      -g 120 -pix_fmt yuv420p \
+      -c:a aac -b:a 128k \
+      -f flv "$RTMP"
+
+    echo "Switching..."
+    sleep 0.5
+  done < videos.txt
 done
